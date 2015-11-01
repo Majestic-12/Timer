2d54e52d1981bddf947c66ffe23a443e1ea0569a
engineering <engineering@Majestic12.local>
2015-10-31 22:23:51 -0700

    Initial Commit

---
+++ Timer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Timer
+//
+//  Created by engineering on 10/31/15.
+//  Copyright (c) 2015 Dan Park. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Timer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Timer
+//
+//  Created by engineering on 10/31/15.
+//  Copyright (c) 2015 Dan Park. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
